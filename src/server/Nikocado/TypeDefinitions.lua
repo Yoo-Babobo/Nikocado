cacd175751cfae7d0d38abc6fb588fc4dce62fea
oofblocks <80658332+oofblocks@users.noreply.github.com>
2022-01-15 18:46:45 -0500

    Add files via upload

---
+++ src/server/Nikocado/TypeDefinitions.lua
@@ -0,0 +1,11 @@
+--// yes
+
+export type NikocadoListener = {
+    Active: bool,
+    Callback: any?
+}
+
+export type Mukbang = {
+    Instance: Instance,
+    NikocadoListener: NikocadoListener
+}
